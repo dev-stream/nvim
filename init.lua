e71f02d12b146f4349aab07a16cb2411ad5b7cb7
Ernesto dev-stream <ernesto.dev.stream@gmail.com>
2024-03-11 16:07:24 +0800

    dev-stream for msys2/cygwin

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+-- faster checkhealth
+vim.api.nvim_set_var("python3_host_prog", "/ucrt64/bin/python3")
+vim.api.nvim_set_var("loaded_python3_provider", 0)
+vim.api.nvim_set_var("loaded_perl_provider", 0)
+vim.api.nvim_set_var("loaded_node_provider", 0)
+
 require("config.lazy")
