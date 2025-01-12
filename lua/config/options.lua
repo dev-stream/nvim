d1a6bb6845b0de992fb81295bdca4b8fbe62ee10
Ernesto dev-stream <ernesto.dev.stream@gmail.com>
2025-01-12 16:15:33 +0800

    options for msys2/cygwin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,37 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+vim.opt.backup = false
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+vim.opt.smarttab = true
+vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" })
+vim.opt.wildignore:append({ "*/node_modules/*" })
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append({ "r" })
+
+-- LF not CRLF
+vim.opt.fileformats = "unix,dos"
+vim.opt.swapfile = false -- Currently trying other vim Distros
+
+if vim.env.MSYSTEM == "UCRT64" then
+  -- msys2 posix environment !!
+  vim.opt.shell = "bash"
+  vim.opt.shellcmdflag = "--login -c" -- `-i` interactive gives issues while installing plugins.
+
+  -- Tested `which bash` and `ll` custom `ls` alias in ~/.bashrc
+  vim.opt.shellxquote = "("
+  vim.opt.shellxescape = "^"
+
+  vim.opt.shellquote = ""
+  vim.opt.shellpipe = "2>&1| tee"
+  vim.opt.shellredir = ">%s 2>&1"
+  vim.opt.shellslash = true -- Unfortunately it gives issues with the Obsidian plugin
+end
